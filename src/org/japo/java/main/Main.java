a47dc6eafbde5ff590ba9053b94cf0dfe27d3573
victo <victo@192.168.1.134>
2019-10-09 23:06:42 +0200

    :tada: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,37 @@
+/* 
+ * Copyright 2019 Elísabet Palomino .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Elísabet Palomino
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        final double CAMBIO_EURO_DOLAR= 1.0921;
+        
+        System.out.printf (Locale.ENGLISH, "El cambio de hoy es 1€ por %.2f$ \n",CAMBIO_EURO_DOLAR);    //System es una clase,out es un recurso de la clase System. el punto indica el recurso.  
+           
+             
+        
+        
+    }//main
+    
+}//class
